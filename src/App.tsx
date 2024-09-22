acf156add6e6144c33384cb1c42dd5ac41bf0169
bandhuShekhar <tiwaribandhushekhar@gmail.com>
2024-09-22 12:57:55 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    Container,
+    createTheme,
+    CssBaseline,
+    ThemeProvider,
+} from "@mui/material";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/Routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
+
+const App = () => {
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Container>
+				<RouterProvider router={router} />
+			</Container>
+		</ThemeProvider>
+	);
+};
 
 export default App;
